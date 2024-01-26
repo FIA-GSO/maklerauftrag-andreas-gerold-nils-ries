a28e0414203c5e2f1e7223fd797847c06fa3fabd
NilsGSO <157117317+NilsGSO@users.noreply.github.com>
2024-01-26 14:46:36 +0100

    Nils, Andreas

---
+++ Andreas Nils Marklerporjekt.py
@@ -0,0 +1,15 @@
+
+
+print("Bitte füllen Sie den Kopf aus")
+print("Wie lautet der Maklername:")
+      name = int(input());
+print (" Wie heißt die Wohnung, welche Sie gerade besichtigen?")
+objekt = int(input());
+print("Welchen Tag ist heute?")
+datum = int (input());
+
+print("Wie viele Räume hat die Wohnung?")
+raeume = int();
+i=1
+while i < raeume
+    print("Handelt es sich um einen komplexen Raum? Antworten Sie mit "Ja" oder "Nein"")
